0916bcb77a2c30602e9d4e3609aeaff9dee1d3a9
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-24 23:32:09 +0000

    implement model Payment  with relations

---
+++ app/Models/Payment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'colocation_id',
+        'from_user_id',
+        'to_user_id',
+        'amount',
+        'paid_at',
+    ];
+
+    protected $casts = [
+        'paid_at' => 'datetime',
+        'amount' => 'decimal:2',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function fromUser()
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function toUser()
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+}
