65b4472eaf2d0417bf3f2555c3ef79d6ab10a95a
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-24 23:26:14 +0000

    implement model Membership  with relations

---
+++ app/Models/Membership.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'colocation_id',
+        'role',
+        'joined_at',
+        'left_at',
+    ];
+
+    protected $casts = [
+        'joined_at' => 'datetime',
+        'left_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
