fe38974b37cd2ee93bf3a7c687d47d0f0520d489
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-25 15:39:53 +0000

    add calculateBalances & getSettlements

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\BalanceCalculator;
+use App\Services\SettlementOptimizer;
 
 class Colocation extends Model
 {
@@ -43,5 +45,25 @@ class Colocation extends Model
     public function categories()
     {
         return $this->hasMany(Category::class);
+    }
+
+    /**
+     * Calculate individual balances for all members of the colocation.
+     *
+     * @return array
+     */
+    public function calculateBalances()
+    {
+        return (new BalanceCalculator())->calculate($this);
+    }
+
+    /**
+     * Suggest minimal settlements to balance the accounts.
+     *
+     * @return array
+     */
+    public function getSettlements()
+    {
+        return new SettlementOptimizer(new BalanceCalculator())->optimize($this);
     }
 }
