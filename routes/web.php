272d03b0965b6854e840dfcf42705ed1d9591119
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-26 21:17:47 +0000

    colocation routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ColocationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // colocation
+    Route::resource('colocations', ColocationController::class);
+    Route::post('colocations/{colocation}/cancel', [ColocationController::class, 'cancel'])->name('colocations.cancel');
+    Route::post('colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('colocations.leave');
+    Route::post('colocations/{colocation}/remove-member/{user}', [ColocationController::class, 'removeMember'])->name('colocations.remove-member');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
