f58f9416c4b9f64622c16255be3c103c48cfab6f
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-26 22:15:34 +0000

    categories & payments routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,6 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::post('invitations/{token}/accept', [InvitationController::class, 'accept'])->name('invitations.accept');
     Route::post('invitations/{token}/refuse', [InvitationController::class, 'refuse'])->name('invitations.refuse');
     Route::resource('colocations.expenses', ExpenseController::class)->only(['store', 'update', 'destroy']);
+    Route::resource('colocations.categories', CategoryController::class)->only(['store', 'update', 'destroy']);
+    Route::post('colocations/{colocation}/payments', [PaymentController::class, 'store'])->name('colocations.payments.store');
     Route::post('colocations/{colocation}/leave', [MembershipController::class, 'leave'])->name('memberships.leave');
     Route::post('colocations/{colocation}/remove-member/{user}', [MembershipController::class, 'remove'])->name('memberships.remove');
 });
