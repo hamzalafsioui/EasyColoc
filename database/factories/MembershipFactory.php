1d53acc81bffa236fad0b7c34b8c76a3f734422c
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-26 19:49:04 +0000

    membership factory

---
+++ database/factories/MembershipFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Colocation;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Membership>
+ */
+class MembershipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'colocation_id' => Colocation::factory(),
+            'role' => 'member',
+            'joined_at' => now(),
+        ];
+    }
+}
