8e09ddc1674784f2ecf6e62a395de9b828b29122
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-27 01:41:01 +0000

    CheckBannedMiddleware

--- app/Http/Middleware/CheckBannedMiddleware.php
+++ app/Http/Middleware/CheckBannedMiddleware.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckBanned
+class CheckBannedMiddleware
 {
     /**
      * Handle an incoming request.
